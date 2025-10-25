75afbf437e4642e4b3e09726c0756d4fcc4c39f1
leventozz <leventufukozturk@gmail.com>
2025-10-25 14:33:45 +0300

    add consumer worker

--- OutboxRelay.Application/Features/Consumers/TransactionConsumedHandler.cs
+++ OutboxRelay.Application/Features/Consumers/TransactionConsumedHandler.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Extensions.Logging;
 using OutboxRelay.Application.Abstractions;
+using OutboxRelay.Common.Messaging;
 using OutboxRelay.Core.Enums;
 using OutboxRelay.Core.Models;
 
 namespace OutboxRelay.Application.Features.Consumers
 {
-    public class TransactionConsumedHandler : IMessageHandler<Transaction>
+    public class TransactionConsumedHandler : IMessageHandler<CreateTransactionMessage>
     {
         private readonly IUnitOfWork _uow;
         private readonly ILogger<TransactionConsumedHandler> _logger;
@@ -16,14 +17,14 @@ namespace OutboxRelay.Application.Features.Consumers
             _logger = logger;
         }
 
-        public async Task HandleAsync(Transaction message, CancellationToken cancellationToken)
+        public async Task HandleAsync(CreateTransactionMessage message, CancellationToken cancellationToken)
         {
             var transaction = await _uow.TransactionRepository.GetByIdAsync(message.Id);
 
             if (transaction == null)
             {
                 _logger.LogError(
-                    "The consumed transaction record could not be found in the database! TransactionId: {TransactionId}",
+                    "The consumed transaction record could not be found in the database. TransactionId: {TransactionId}",
                     message.Id);
                 throw new InvalidOperationException($"Transaction {message.Id} not found in database.");
             }
@@ -38,9 +39,12 @@ namespace OutboxRelay.Application.Features.Consumers
                 return;
             }
 
-            transaction.Status = (short)TransactionStatus.Completed; 
+            transaction.Status = (short)TransactionStatus.Completed;
 
+            //getbyid already tracked by ef core
             //await _uow.TransactionRepository.UpdateStatusAsync(transaction.Id, (short)TransactionStatus.Completed); 
+
+            await _uow.SaveChangesAsync(cancellationToken);
         }
     }
 }
--- OutboxRelay.ConsumerWorkerService/ConsumerWorkerService.cs
+++ OutboxRelay.ConsumerWorkerService/ConsumerWorkerService.cs
@@ -1,23 +1,92 @@
+using OutboxRelay.Application.Features.Consumers;
+using OutboxRelay.Common.Const;
+using OutboxRelay.Common.Messaging;
+using OutboxRelay.Common.Options;
+using OutboxRelay.Core.Models;
+using OutboxRelay.Infrastructure.Publisher;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Channels;
+
 namespace OutboxRelay.ConsumerWorkerService
 {
     public class ConsumerWorkerService : BackgroundService
     {
         private readonly ILogger<ConsumerWorkerService> _logger;
+        private readonly TimeSpan _pollingInterval = TimeSpan.FromSeconds(2);
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly RabbitMqClientService _rabbitMqClientService;
 
-        public ConsumerWorkerService(ILogger<ConsumerWorkerService> logger)
+        public ConsumerWorkerService(ILogger<ConsumerWorkerService> logger, IServiceScopeFactory serviceScopeFactory, RabbitMqClientService rabbitMqClientService)
         {
             _logger = logger;
+            _serviceScopeFactory = serviceScopeFactory;
+            _rabbitMqClientService = rabbitMqClientService;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                while (!cancellationToken.IsCancellationRequested)
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    try
+                    {
+                        await ConsumeAndProcessTransactionMessage(cancellationToken);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "An unexpected error occurred while processing outbox messages.");
+                    }
+
+                    await Task.Delay(_pollingInterval, cancellationToken);
                 }
-                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        private async Task ConsumeAndProcessTransactionMessage(CancellationToken cancellationToken)
+        {
+            var connection = await _rabbitMqClientService.GetConnectionAsync();
+
+            await using var channel = await connection.CreateChannelAsync();
+
+            await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false, cancellationToken: cancellationToken);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            await channel.BasicConsumeAsync(
+                queue: RabbitMqConstants.TransactionQueueName,
+                autoAck: false,
+                consumer: consumer);
+
+            consumer.ReceivedAsync += Consumer_ReceivedAsync;
+        }
+
+        private async Task Consumer_ReceivedAsync(object sender, BasicDeliverEventArgs @event)
+        {
+            var consumer = (AsyncEventingBasicConsumer)sender;
+            var channel = consumer.Channel;
+
+            var createTransactionMessage = JsonSerializer.Deserialize<CreateTransactionMessage>(Encoding.UTF8.GetString(@event.Body.ToArray()),JsonDefaults.Default);
+
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var consumerHandler = scope.ServiceProvider.GetRequiredService<IMessageHandler<CreateTransactionMessage>>();
+                try
+                {
+                    await consumerHandler.HandleAsync(createTransactionMessage, CancellationToken.None);
+                    await channel.BasicAckAsync(deliveryTag: @event.DeliveryTag, multiple: false);
+                }
+                catch (Exception)
+                {
+                    await channel.BasicNackAsync(
+                        deliveryTag: @event.DeliveryTag,
+                        multiple: false,
+                        requeue: false);
+                }
+                
             }
         }
     }
